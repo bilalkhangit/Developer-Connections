d65330607a6c103c1ffc87fd729e14e39876f7d9
Bilal Khan <bilalkhanNew@Outlook.clm>
2018-12-10 13:12:10 +0500

    Routes Comments

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-
+// @route  GET api/profile/test
+// @Desc   Tests users route
+// @access public
 router.get("/test", (req, res) => {
     res.json({
       msg: "Profile Works"
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+
+// @route  GET api/users/test
+// @Desc   Tests users route
+// @access public
 router.get("/test", (req, res) => {
   res.json({
     msg: "User Works"
